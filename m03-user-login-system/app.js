83e5c201a0c19f21db575c5acdfef52a5ce59ddb
Eduardo Chaves <educostachaves@hotmail.com>
2016-02-17 12:29:52 -0200

    user login - user registration

--- m03-user-login-system/app.js
+++ m03-user-login-system/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
+
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
@@ -23,41 +24,41 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// handle fileuploads
+// Handle file uploads
 var upload = multer({dest:'uploads/'});
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// handle Express Sessions
+// Handle Express Sessions
 app.use(session({
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: true
+    secret:'secret',
+    saveUninitialized: true,
+    resave: true
 }));
 
 // passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-//validator
+// Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
-    var namespace = param.split('.'),
-        root = namespace.shift(),
-        formParam = root;
+      var namespace = param.split('.'),
+          root    = namespace.shift(),
+          formParam = root;
 
     while(namespace.length) {
       formParam += '[' + namespace.shift() + ']';
     }
 
     return {
-      param: formParam,
-      msg: msg,
-      value: value
+      param : formParam,
+      msg   : msg,
+      value : value
     };
   }
 }));
@@ -65,8 +66,9 @@ app.use(expressValidator({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use(flash());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
 });
--- m03-user-login-system/routes/users.js
+++ m03-user-login-system/routes/users.js
@@ -7,15 +7,82 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register',{
+  res.render('register', {
     'title': 'Register'
   });
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login',{
+  res.render('login', {
     'title': 'Login'
   });
+});
+
+router.post('/register', function(req, res, next) {
+  var name = req.body.name,
+      email = req.body.email,
+      username = req.body.username,
+      password = req.body.password,
+      password2 =  req.body.password2;
+
+  // Check for Image Field
+  if(req.body.profileimage){
+    console.log('Uploading File...');
+
+    // File Info
+    var profileImageOriginalName = req.body.profileimage.originalname,
+        profileImageName = req.body.profileimage.name,
+        profileImageMime = req.body.profileimage.mimetype,
+        profileImagePath = req.body.profileimage.path,
+        profileImageExt = req.body.profileimage.extension,
+        profileImageSize = req.body.profileimage.size;
+
+  } else {
+    // Set a Default Image
+    var profileImageName = 'noimage.png';
+  }
+
+  // Form Validation
+  req.checkBody('name','Name field is required').notEmpty();
+  req.checkBody('email','Email field is required').notEmpty();
+  req.checkBody('email','Email not valid').isEmail();
+  req.checkBody('username','Username field is required').notEmpty();
+  req.checkBody('password','Password field is required').notEmpty();
+  req.checkBody('password2','Password do not match').equals(req.body.password);
+
+  // Check for errors
+  var errors = req.validationErrors();
+
+  if(errors){
+    res.render('register', {
+      errors: errors,
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      password2: password2
+    });
+  } else {
+    var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileImage: profileImageName
+    });
+
+    // Create User
+    // User.createUser(newUser, function(err, user){
+    //     if(err)throw err;
+    //     console.log(user);
+    // });
+
+    //Success Message
+    req.flash('success', 'You are noew registered and may log in');
+
+    res.location('/');
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
